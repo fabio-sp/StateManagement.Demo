2d07f597133834d63d35ecdfd1ccd10a10bd2b9c
Fabio <fabio.spaziani@ellycode.com>
2024-01-21 17:41:55 +0100

    add icollectionstateservice to services

--- StateManagement.Demo/Program.cs
+++ StateManagement.Demo/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddFluxor(opts =>
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<ICollectionService, CollectionService>();
+builder.Services.AddScoped<ICollectionStateService, CollectionStateService>();
 builder.Services.AddScoped<IConversationService, ConversationService>();
 
 await builder.Build().RunAsync();
